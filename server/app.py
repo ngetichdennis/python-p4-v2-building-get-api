ecd81fcd9de0e5fa817128566582d27f18678ec2
Dennis <ngetichdennis90@gmail.com>
2024-04-19 22:55:18 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -47,6 +47,7 @@ def game_by_id(id):
     return response
 @app.route('/games/users/<int:id>')
 def game_users_by_id(id):
+    #filtering by id
     game = Game.query.filter(Game.id == id).first()
     # use association proxy to get users for a game
     users = [user.to_dict(rules=("-reviews",)) for user in game.users]
